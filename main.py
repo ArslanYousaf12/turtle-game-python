ee7d0323ff9a76031ece8ef93d0923d34a50ff5f
Arslan Yousaf <devtechofficial90@gmail.com>
2025-03-13 14:53:10 +0500

    complete turtle race game

--- main.py
+++ main.py
@@ -1,43 +1,43 @@
 from turtle import Turtle, Screen
+import random
 
+screen = Screen()
+screen.screensize(canvwidth=500, canvheight=400)
+user_bet =screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter the color ")
+print(user_bet)
+colors_list = ["red", "orange", "green", "purple", "blue", "yellow"]
+turtle_list = []
+gap = 0
+is_game_on = False
+for num in range(6):
 
-timy = Turtle()
-my_screen = Screen()
-
-
-
-
-
-
-def move_forward():
-    timy.forward(10)
+    new_turtle = Turtle("turtle")
+    new_turtle.penup()
+    new_turtle.color(colors_list[num])
+    new_turtle.goto(-350  , -150 + gap )
+    gap += 40
+    turtle_list.append(new_turtle)
     
+
+if user_bet:
+    is_game_on = True
+
+while is_game_on:
+    for turtle in turtle_list:
+        if turtle.xcor() > 330:
+            is_game_on = False
+            wining_color = turtle.pencolor()
+            if wining_color == user_bet:
+                print(f"You won! the wining color is {wining_color}")
+            else:
+                print(f"You loose! The wining color is {wining_color}")
+        distance = random.randint(1,10)
+        turtle.forward(distance)
     
-def move_backward():
-    timy.backward(10)
-
-def turn_left():
-    timy.left(10)
-
-def turn_right():
-    timy.right(10)
-
-def clear():
-    timy.clear()
-    timy.penup()
-    timy.home()
-    timy.pendown()
-    timy.setheading(0)
-
-my_screen.listen()
-
-my_screen.onkey(key="w", fun=move_forward)
-my_screen.onkey(key="s", fun=move_backward)
-my_screen.onkey(key="a", fun=turn_left)
-my_screen.onkey(key="d", fun=turn_right)
-my_screen.onkey(key="c", fun=clear)
 
 
 
 
-my_screen.exitonclick()
+
+
+screen.exitonclick()

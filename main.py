f21d21e8f3940f2a0fc2f668ac7cc4c92c9e13cb
Arslan Yousaf <devtechofficial90@gmail.com>
2025-03-13 12:03:10 +0500

    created strach game

--- main.py
+++ main.py
@@ -11,11 +11,31 @@ my_screen = Screen()
 
 def move_forward():
     timy.forward(10)
+    
+    
+def move_backward():
+    timy.backward(10)
 
+def turn_left():
+    timy.left(10)
+
+def turn_right():
+    timy.right(10)
+
+def clear():
+    timy.clear()
+    timy.penup()
+    timy.home()
+    timy.pendown()
+    timy.setheading(0)
 
 my_screen.listen()
 
-my_screen.onkey(key="space", fun=move_forward)
+my_screen.onkey(key="w", fun=move_forward)
+my_screen.onkey(key="s", fun=move_backward)
+my_screen.onkey(key="a", fun=turn_left)
+my_screen.onkey(key="d", fun=turn_right)
+my_screen.onkey(key="c", fun=clear)
 
 
 
